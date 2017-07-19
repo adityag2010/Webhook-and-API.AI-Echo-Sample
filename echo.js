14e4f54cf46df25bea089943bfbf69f4bdce3371
Aditya Gupta <adityag2010@gmail.com>
2017-07-19 13:44:57 +0530

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,10 +1,10 @@
 'use strict';
 
-/*const express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const restService = express();
-*/
+
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
